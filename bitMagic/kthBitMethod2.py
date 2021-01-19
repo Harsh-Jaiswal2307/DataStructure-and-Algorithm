435c7e19648f529292b137008934feeca77218de
Harsh Jaiswal <66635990+Harsh-Jaiswal2307@users.noreply.github.com>
2021-01-19 15:29:32 +0530

    Update kthBitMethod2.py

--- bitMagic/kthBitMethod2.py
+++ bitMagic/kthBitMethod2.py
@@ -1,1 +1,22 @@
+"""
+METHOD 2: Using the right shift
+Algorithm:
+  Step 1: Right shift n by k-1 (n >> (k-1))
+  Step 2: if last bit is 1, then kth bit is SET else NOT SET
+  
+EX:
+n = 27 k = 4
+(27) = (11011)
+n >> k-1 = 27 >> 3 = 00011
+and finally, 
+((00011)&1) to check if kth bit is set or not
+"""
+
+n, k = map(int, input().split())
+n = n >> (k-1)
+if (n & 1) == 0:
+  print("UNSET")
+else:
+  print("SET")
+  
 
